f98041adc3fbe0051503c84c547f36b69e438c34
Mick Staugaard <mick@staugaard.com>
2009-11-26 15:22:26 -0800

    remove from cache after update

--- lib/cache_back/write_mixin.rb
+++ lib/cache_back/write_mixin.rb
@@ -23,6 +23,7 @@ module CacheBack
 
     def self.included(model_class)
       #model_class.after_save :store_in_cache_back
+      model_class.after_update :remove_from_cache_back
       model_class.after_destroy :remove_from_cache_back
     end
   end
